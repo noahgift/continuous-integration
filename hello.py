6c8f5400bbcc9f6bf342020f0b28f280a8f263e2
Noah Gift <noah.gift@gmail.com>
2022-03-03 01:14:59 +0000

    adding structure

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+def add(x, y):
+    return int(x + y)
+
+
+print(add(1, 1))
+#var=1
+#var=var
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import add
+
+
+def test_add():
+    assert 2 == add(1, 1)
